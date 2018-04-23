a207e02943c4678c9f9774414a53781a180134d7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/E2096.Module/Module.vb
@@ -0,0 +1,13 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports DevExpress.ExpressApp
+
+Namespace E2096.Module
+    Partial Public NotInheritable Class E2096Module
+        Inherits ModuleBase
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace

2b8d6d9504be3dd3a14a0368f3221bd047e71421
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HyperLinkPropertyEditor.Web/Module.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+
+namespace HyperLinkPropertyEditor.Web {
+    public sealed partial class HyperLinkPropertyEditorAspNetModule : ModuleBase {
+        public HyperLinkPropertyEditorAspNetModule() {
+            InitializeComponent();
+        }
+    }
+}
